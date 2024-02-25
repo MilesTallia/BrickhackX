7ffce68995a1ea80e51809822a6d0579872265f7
Miles Tallia (RIT Student) <mot7506@rit.edu>
2024-02-25 09:31:15 -0500

    enemy shit

--- ArcaneOasisGame/Assets/Scripts/EnemyDamage.cs
+++ ArcaneOasisGame/Assets/Scripts/EnemyDamage.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour {
     public PlayerHealth playerHealth;
+    public PlayerHealth enemyHealth;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player") {
             Debug.Log("Hit player");
             playerHealth.TakeDamage(100);
+            enemyHealth.TakeDamage(10);
         }
     }
 }
